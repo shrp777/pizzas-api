3da30bc54a894468a9a8906a26fafcda4b3e3507
Alexandre Leroux <alex@sherpa.one>
2022-11-29 11:45:43 +0100

    hot reloading + health check

--- README.md
+++ README.md
@@ -6,6 +6,43 @@ Projet modèle d'emploi de Docker pour la mise en place d'une architecture Back 
 - Tutoriel officiel :
 https://fastapi.tiangolo.com/deployment/docker/
 
+## Commandes Docker utiles
+
+- Création d'une image à partir du fichier Dockerfile et des sources :
+`docker build -t fastapiimage .`
+
+- Création d'un container à partir de l'image "fastapiimage" précédemment créée :
+`docker run -d --name fastapi -p 80:80 fastapiimage`
+
+- Initialisation des services :
+`docker compose up`
+
+- Démarrage des services :
+`docker compose start`
+
+- Consultation des services actifs :
+`docker compose ps`
+
+- Clôture des services : 
+`docker compose stop`
+
+## Test de l'API
+
+- Racine : 
+
+http://localhost/
+
+- Route items :
+
+http://localhost/items
+
+http://localhost/items/10
+
+http://localhost/items/10?q=test
+
+- Documentation Swagger générée automatiquement
+http://localhost/docs
+
 ---
 
 **Alexandre Leroux**
---
+++ api/app/main.py
@@ -0,0 +1,76 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+import os
+import time
+
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://fastapi:fastapi@db:5432/fastapi"
+
+def wait_for_db(db_uri):
+    """checks if database connection is established"""
+
+    _local_engine = create_engine(db_uri)
+
+    _LocalSessionLocal = sessionmaker(
+        autocommit=False, autoflush=False, bind=_local_engine
+    )
+
+    up = False
+    while not up:
+        try:
+            # Try to create session to check if DB is awake
+            db_session = _LocalSessionLocal()
+            # try some basic query
+            db_session.execute("SELECT 1")
+            db_session.commit()
+        except Exception as err:
+            print(f"Connection error: {err}")
+            up = False
+        else:
+            up = True
+        
+        time.sleep(2)
+
+#wait_for_db(SQLALCHEMY_DATABASE_URL)
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+app = FastAPI()
+
+
+class Team(Base):
+    __tablename__ = "teams"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Test Fast API"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/items")
+def read_items():
+    return []
+
