9c63fbff465146ddbfea454bcae18390a6e31964
SHRP <alex@sherpa.one>
2025-02-14 11:40:55 +0100

    ajouter user

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from .routers import pizzas
+from .routers import users
 
 app = FastAPI()
 app.include_router(pizzas.router)
+app.include_router(users.router)
+
 
 @app.get("/")
 def read_root():
